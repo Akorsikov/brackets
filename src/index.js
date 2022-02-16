48fec92a6f4133ba62c66d78f87fbee091904e9a
Akorsikov <akors@mail.ru>
2022-02-17 00:47:17 +0300

    feat: code 13passing 7failed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  if (str.length % 2 !== 0) return false;
+
+  let array = [...str];  
+  let indexFirstCloser;
+  let indexCurrent;
+  let kindBrackets;
+
+  while (array.length > 0) {
+    
+    indexFirstCloser = array.length - 1;
+    
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      indexCurrent = array.indexOf((bracketsConfig[i])[1]);
+      
+      if (indexCurrent > 0 &&
+        indexFirstCloser >= indexCurrent) {
+        indexFirstCloser = indexCurrent;
+        kindBrackets = i;
+        
+      }
+      
+    }
+    
+    if (array[indexFirstCloser - 1] === bracketsConfig[kindBrackets][0]) {
+      array.splice(indexFirstCloser - 1, 2);      
+    } else return false;
+  }
+  return true;
 }
+//console.log(check('([{}])', [['(', ')'], ['[', ']'], ['{', '}']]));
